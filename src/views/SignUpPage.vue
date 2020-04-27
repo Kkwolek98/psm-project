06f7eddbd7dcbc1c3e183853186eb63ee26de551
Katarzyna Knapik <katarzyna_knapik@onet.pl>
2020-04-27 21:55:26 +0200

    add password comparison

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -8,7 +8,7 @@
             <label for="signup-password">Password</label><br>
             <input type="password" id="signup-password" v-model="password" required><br><br>
             <label for="signup-repassword">Confirm Password</label><br> 
-            <input type="password" id="signup-pepassword"  required>
+            <input type="password" id="signup-repassword" v-model="repassword" v-on:input="passwordCompare" required>
             
             <br><br>
             <button v-on:click="registerUser">Sign up</button>
@@ -29,21 +29,33 @@
             return{
                 email: '',
                 password: '',
+                repassword: '',
                 error: ''
             }
         },
         methods: {
             registerUser(e){
-                console.log(firebase.auth)
-                console.log(this.email, this.password)
-                e.preventDefault();
-                firebase.auth.createUserWithEmailAndPassword(this.email, this.password).then(user => {
-                    console.log(user.user)    
-                })
-                .catch(error => {
-                    this.error = error
-                }) 
+                if(this.passwordCompare()){
+                    console.log(firebase.auth)
+                    console.log(this.email, this.password)
+                    e.preventDefault();
+                    firebase.auth.createUserWithEmailAndPassword(this.email, this.password).then(user => {
+                        console.log(user.user)    
+                    })
+                    .catch(error => {
+                        this.error = error
+                    }) 
+                }
+            },
+            passwordCompare(){
+                if(this.password != this.repassword){
+                    this.error = "Passwords don't match"
+                    return false
+                }
+                this.error = ''
+                return true
             }
+
         }
          
     }
