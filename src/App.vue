74a52a936e653b1ec4ebd4bd3d0b41360729fde7
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-04-28 23:10:10 +0200

    basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
+    <HeaderTitle />
+    <router-view class="router" />
+
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/findItem">Find Item</router-link> |
-      <router-link to="/login">Log In</router-link> | 
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/findItem">Find Item</router-link>|
+      <router-link to="/login">Log In</router-link>|
       <router-link to="/signup">Sign Up</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+$primary-font-color: #959595;
+$secondary-font-color: rgba(110, 226, 245, 0.9);
+$button-font-color: #f3f3f3;
+$small-font-color: #5e5e5e;
+
+$button-gradient-first: #6ee2f5;
+$button-gradient-second: #6454f0;
+
+.router {
+  padding-top: 122px;
+  z-index: 4;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
@@ -25,11 +38,97 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $primary-font-color;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $secondary-font-color;
     }
   }
 }
+
+body {
+  background: #1c1c1c;
+  color: $primary-font-color;
+}
+
+.custom-button--large,
+button {
+  transition: all 0.4s ease-in-out;
+  height: 42px;
+  background-image: linear-gradient(
+    105deg,
+    $button-gradient-first 9%,
+    $button-gradient-second 91%
+  );
+  width: 130px;
+  border: 0;
+  border-radius: 20px;
+  color: $button-font-color;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    background-image: linear-gradient(
+      105deg,
+      darken($button-gradient-first, 3) 9%,
+      darken($button-gradient-second, 6) 91%
+    );
+    transition: all 0.8s ease-in-out;
+    -webkit-transition: all 0.8s ease-in-out;
+  }
+}
+
+.box {
+  border-radius: 12px;
+  background-color: rgb(46, 46, 46);
+  padding: 28px;
+  z-index: 10;
+}
+
+.box-center {
+  width: 15%;
+  margin: 0 auto;
+}
+@media (max-width: 1350px) {
+  .box-center {
+    width: 28%;
+  }
+}
+@media (max-width: 750px) {
+  .box-center {
+    width: 55%;
+  }
+}
+
+input {
+  width: 100%;
+  display: block;
+  border: 0;
+  height: 38px;
+  border-radius: 8px;
+  background-color: rgba(119, 119, 119, 0.2);
+  margin-bottom: 32px;
+  color: $primary-font-color;
+}
+
+.clickable {
+  cursor: pointer;
+}
+
+.font-12 {
+  color: $small-font-color;
+  font-size: 12px;
+}
+.font-12--important {
+  color: $button-font-color;
+  font-size: 12px;
+}
 </style>
+
+<script>
+import HeaderTitle from "@/components/HeaderTitle.vue";
+export default {
+  components: {
+    HeaderTitle
+  }
+};
+</script>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="container">
-        <h1>Login</h1>
-
-        <form class="login-form">
-            <label for="login-email">Username or Email</label><br> 
-            <input type="email" id="login-email" required><br><br>
-            <label for="login-password">Password</label><br>
-            <input type="password" id="login-password" required>
-            <br><br>
-            <button>Log in</button>
-        </form>
-        
-    </div>
+  <div class="container">
+    <form class="login-form box box-center">
+      <label for="login-email">Username or Email</label>
+      <input type="email" id="login-email" required />
+      <label for="login-password">Password</label>
+      <input type="password" id="login-password" required />
+      <button>Log in</button>
+      <p class="font-12">or</p>
+      <router-link to="/signup" class="font-12--important clickable">create an account</router-link>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'LoginPage',
-         
-    }
+export default {
+  name: "LoginPage"
+};
 </script>
+
+<style lang="scss">
+</style>
